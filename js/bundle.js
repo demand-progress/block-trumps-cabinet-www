1bee4e1d20324138f945b320b28502ace3e4a562
kellie-henning <kellie.henning@gmail.com>
2016-12-05 17:36:26 -0800

    Wired up email share button

--- js/bundle.js
+++ js/bundle.js
@@ -917,7 +917,8 @@
 	                ),
 	                React.createElement(
 	                    'a',
-	                    { target: '_blank', href: '#Share via Email', className: 'email' },
+	                    { href: 'mailto:?subject= I%20just%20signed%20this%3A &body= Hi%20-%20I%20just%20took%20action%20against%20Donald%20Trump\u2019s%20horrifying%20picks%20for%20cabinet-level%20roles%20in%20his%20administrations. %0A%0A Trump\u2019s%20nominees%20have%20promoted%20white%20nationalism%2C%20attacked%20climate%20science%20and%20used%20their%20power%20as%20Wall%20Street%20insiders%20to%20fleece%20working%20families. %0A%0A I%20just%20signed%20a%20petition%20urging%20the%20Senate%20to%20block%20and%20resist%20any%20Trump%20nominee%20embracing%20hatred%20and%20greed.%20Could%20you%20sign%20too%3F %0A%0A https%3A%2F%2Fwww.BlockTrumpsCabinet.com%2F%3Fsource%3Dtw-share',
+	                        target: '_blank', className: 'email' },
 	                    'Email'
 	                )
 	            )
